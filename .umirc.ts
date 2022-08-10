1dbe77c92887c74202421eabb8b8ec23f836b4a0
liangsky <640634387@qq.com>
2022-08-10 12:59:34 +0800

    feat:改用@liangskyli/mock 方案

--- .umirc.ts
+++ .umirc.ts
@@ -29,11 +29,12 @@ export default defineConfig({
     qiankun: {
         slave: {},
     },
-    mock: {
+    mock: false,
+    /*mock: {
         // umi3要排除，这不是mock入口，umi4排除了，改这里的文件，不热更新，umi3没问题
         // umi4注释后，mock/custom-data/template-data.ts 文件更改，需要改2次，才有第一次更改的数据，延迟了一次
-        //exclude: ['mock/custom-data/**'],
-    }
+        //exclude: ['mock/custom-data/!**'],
+    }*/
 });
 
 
--- plugin.ts
+++ plugin.ts
@@ -1,5 +1,7 @@
 // https://github.com/umijs/umi-next/blob/master/examples/boilerplate/plugin.ts
 import type { IApi } from 'umi';
+import {getMiddleware} from "@liangskyli/mock";
+import path from "path";
 
 export default (api: IApi) => {
   api.modifyHTML(($) => {
@@ -10,4 +12,24 @@ export default (api: IApi) => {
 
     return $;
   });
+  if (api.name === 'dev' && process.env.MOCK !== 'false') {
+    // 只有 dev 才走mock
+    // mock 开关配置判断
+    api.addBeforeMiddlewares(async () => {
+      const mockDir = path.join(process.cwd(), './mock');
+      require('ts-node').register({
+        dir: mockDir,
+        emit: false,
+        transpileOnly: true,
+        compilerOptions: {
+          allowJs: true,
+        },
+      });
+
+      const { middleware } = await getMiddleware({exclude: ['mock/custom-data/**']});
+      return [
+        middleware,
+      ];
+    });
+  }
 };
